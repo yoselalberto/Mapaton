8e3a5d888f98d1fd730a4bc247c8ef466f4f2605
Alberto <yoselerratil@gmail.com>
2016-03-06 07:05:13 -0600

    cambios menores

--- Analisis/GTFS_2_shape.R
+++ Analisis/GTFS_2_shape.R
@@ -4,9 +4,28 @@ library(sp)
 library(rgdal)
 library(dplyr)
 library(ggplot2)
+library(maps)
+library(maptools)
 
 # cargo los datos
-shapes <- read.csv("Datos/mapatonGTFS/shapes.txt", header = TRUE,
-                   row.names = NULL)
+shape <- read.csv("Datos/mapatonGTFS/shapes.txt", header = TRUE, row.names = NULL,
+                  as.is = TRUE)
 
-# visualizacion
+# preprocesado
+ids <- unique(shape$shape_id)
+shape_orig <- shape
+shape <- shape[shape$shape_id == ids[1],] # subset the data
+
+#
+shape_map <- list(x = shape$shape_pt_lon, y = shape$shape_pt_lat)
+shape_lines <- map2SpatialLines(shape_map, IDs = ids[1])
+plot(shape_lines) # success - this plots a single line!
+
+for(i in 2:length(ids)){
+    shape <- shape_orig[shape_orig$shape_id == ids[i],]
+    shape_map <- list(x = shape$shape_pt_lon, y = shape$shape_pt_lat)
+    shape_temp <- map2SpatialLines(shape_map, IDs = ids[i])
+    shape_lines <- spRbind(shape_lines, shape_temp)
+}
+# guardo el mapa en rds
+saveRDS(shape_lines, "Datos/Rutas_mapaton.Rds", compress = FALSE)
