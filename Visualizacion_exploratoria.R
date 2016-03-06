3de947119d5863496b0fdf6bd50d48e32d42bf1a
Alberto <yoselerratil@gmail.com>
2016-03-06 01:13:04 -0600

    Visualizare las rutas en leaflet

---
+++ Visualizacion_exploratoria.R
@@ -0,0 +1,28 @@
+# Visualizacion exploratoria
+setwd("~/Datos/Morlan/Mapaton/Analisis")
+library(sp)
+library(rgdal)
+library(dplyr)
+library(leaflet)
+
+# cargado de datos
+estatal <- readOGR(dsn = "Datos", layer = "df_estatal") %>%
+           geometry()
+delegaciones <- readOGR(dsn = "Datos", layer = "df_municipal") %>%
+           geometry()
+rutas <- readRDS("Datos/Rutas_viales.Rds")
+
+
+
+# mapa en leaflet
+mapa <- leaflet() %>%
+        setView(-99.168, 19.383, zoom = 12) %>%
+        addProviderTiles("CartoDB.PositronNoLabels",
+                         options = tileOptions(minZoom = 11, maxZoom = 14)) %>%
+        setMaxBounds(-99.3649, 19.0482,-98.9403, 19.70) %>%
+        addPolygons(data = estatal, color = "darkgrey", fill = FALSE, opacity = 1,
+                    weight = 4) %>%
+        addPolygons(data = delegaciones, weight = 2, color = "darkgrey", 
+                    fill = FALSE, opacity = 1) %>%
+        addPolylines(data = rutas, color = "#7f7f7f", opacity = 0.95, weight = 1.5)
+mapa
