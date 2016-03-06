14f1bdae2cf04b9f06a892451875fece1e8b494a
Alberto <yoselerratil@gmail.com>
2016-03-06 08:26:26 -0600

    mapeare la densidad de rutas por ageb

---
+++ Analisis/Rutas_ageb.R
@@ -0,0 +1,44 @@
+# exploro las rutas por ageb
+setwd("~/Datos/Morlan/Mapaton/Analisis")
+library(sp)
+library(rgdal)
+library(jsonlite)
+library(dplyr)
+library(leaflet)
+
+# cargado de datos
+estatal <- readOGR(dsn = "Datos", layer = "df_estatal") %>%
+           geometry()
+delegaciones <- readOGR(dsn = "Datos", layer = "df_municipal") %>%
+           geometry()
+# rutas mapaton
+mapaton <- readRDS("Datos/Rutas_mapaton.Rds")
+# ageb
+ageb <- readOGR("Datos", layer = "df_ageb_urb")
+ageb <- ageb[, "CVEGEO"]        
+densidades <- fromJSON("Datos/JSONes/agebDen.json")
+
+# macheo las ageb's con las rutas de transporte
+orden <- apply(sapply(ageb[["CVEGEO"]], FUN = "==", densidades$id),
+                    MARGIN = 2, FUN = which)
+ageb$densidad <- unlist(densidades[orden, "densidad"])
+
+
+# visualizo las densidades en leaflet
+mapa_ageb <- leaflet(data = ageb) %>%
+            setView(-99.168, 19.383, zoom = 12) %>%
+            addProviderTiles("CartoDB.PositronNoLabels",
+                             options = tileOptions(minZoom = 11, maxZoom = 14)) %>%
+            setMaxBounds(-99.3649, 19.0482,-98.9403, 19.6) %>%
+            addPolygons(data = estatal, color = "darkgrey", fill = FALSE, opacity = 1,
+                        weight = 4) %>%
+            addPolygons(data = delegaciones, weight = 2, color = "darkgrey", 
+                        fill = FALSE, opacity = 1) %>%
+            addPolygons(color = "#B40404", fillOpacity = ageb$densidad, weight = 1.5,
+                        stroke = FALSE) %>%
+            addPolylines(data = mapaton, color = "#0431B4", opacity = 0.85, weight = 0.65)
+mapa_ageb
+
+
+
+
