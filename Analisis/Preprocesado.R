b56d6dfbfd1305ef2a7c03c38ed7644ff5d082e2
Alberto <yoselerratil@gmail.com>
2016-03-06 01:26:48 -0600

    comento codigo no necesario

--- Analisis/Preprocesado.R
+++ Analisis/Preprocesado.R
@@ -10,13 +10,10 @@ estatal <- readOGR(dsn = "Datos", layer = "df_estatal") %>%
 delegaciones <- readOGR(dsn = "Datos", layer = "df_municipal") %>%
                 geometry()
 
-eje_vial <- readOGR(dsn = "Datos", layer = "df_eje_vial") %>%
-            geometry()
+#eje_vial <- readOGR(dsn = "Datos", layer = "df_eje_vial") %>% geometry()
 # mapas
 par(mar = c(0, 0, 0, 0) + 0.1, xaxs = "i", yaxs = "i",  lwd = 0.3, cex = 2, 
     family = "serif", bg = "gray98")
 plot(estatal)
 plot(delegaciones, add = TRUE)
-plot(eje_vial, add = TRUE, col = "gray20")
-
-
+#plot(eje_vial, add = TRUE, col = "gray20")
